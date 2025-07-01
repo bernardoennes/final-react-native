d24c68953c17a4cfcc8df0699de16583834bec79
João Pedro B. Dias <jotape_dias@outlook.com>
2025-07-01 19:55:41 -0300

    mudanças na navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,17 +2,15 @@ import React from "react";
 import styles from "./navbar-styles"
 import { Image, TouchableOpacity, View } from "react-native";
 import logo from "../assets/logo.png"
-import { GearSix, House } from "phosphor-react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export function NavBar() {
+    const navigation = useNavigation();
+
     return (
         <View style = {styles.container}>
-            <TouchableOpacity style = {styles.button}>
-                <House size={32} weight="fill"/>
-            </TouchableOpacity>
-            <Image source={logo} style = {styles.logo}/>
-            <TouchableOpacity style = {styles.button}>
-                <GearSix size={32} weight="fill"/>
+            <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+                <Image source={logo} style = {styles.logo}/>
             </TouchableOpacity>
         </View>
     );
